9b6cc9906e619f24bd9f4b8232c91436e31a8e63
ymougenel <yann.mougenel@gmail.com>
2018-08-19 16:23:20 +0200

    refactor(Test): prepare test structure

--- src/main/kotlin/com/ymougenel/myMetrics/controllers/MetricControllerI.kt
+++ src/main/kotlin/com/ymougenel/myMetrics/controllers/MetricControllerI.kt
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping
 interface MetricControllerI {
 
     // ---------------------------------  CRUD  ---------------------------------
-    // TODO
     @PostMapping("/TODO")
     fun create(metric: Metric)
 
---
+++ src/test/kotlin/com/ymougenel/myMetrics/controllers/metrics/CrudTest.java
@@ -0,0 +1,55 @@
+package com.ymougenel.myMetrics.controllers.metrics;
+
+import org.junit.Test;
+
+public class CrudTest {
+
+    // TODO test create
+    @Test
+    public void create_succeed() {
+        assert(false);
+    }
+
+    // TODO test create
+    @Test
+    public void create_fail() {
+        assert(false);
+    }
+
+    // TODO test getByID
+    @Test
+    public void getByID_succeed() {
+        assert(false);
+    }
+
+    // TODO test getByID
+    @Test
+    public void getByID_fail() {
+        assert(false);
+    }
+
+    // TODO test update
+    @Test
+    public void update_succeed() {
+        assert(false);
+    }
+
+    // TODO test update
+    @Test
+    public void update_fail() {
+        assert(false);
+    }
+
+    // TODO test delete
+    @Test
+    public void delete_succeed() {
+        assert(false);
+    }
+
+    // TODO test delete
+    @Test
+    public void delete_fail() {
+        assert(false);
+    }
+
+}
