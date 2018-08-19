13cdfa58d595d8beadaaf00313d09eba95a402c1
ymougenel <yann.mougenel@gmail.com>
2018-08-19 15:24:29 +0200

    feat: start the API facade

---
+++ src/main/kotlin/com/ymougenel/myMetrics/models/MetricTypes.java
@@ -0,0 +1,4 @@
+package com.ymougenel.myMetrics.models;
+
+public enum MetricTypes {
+}
---
+++ src/main/kotlin/com/ymougenel/myMetrics/models/mocks/MetricMock.java
@@ -0,0 +1,8 @@
+package com.ymougenel.myMetrics.models.mocks;
+
+import com.ymougenel.myMetrics.models.Metric;
+
+public class MetricMock {
+
+    public static Metric get() { return new Metric();}
+}
