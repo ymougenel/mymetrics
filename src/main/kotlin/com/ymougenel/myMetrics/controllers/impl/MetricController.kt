aec3c20bfd76c3896c765976ad5d50daf43d1073
ymougenel <yann.mougenel@gmail.com>
2018-08-31 17:56:34 +0200

    wip

--- src/main/kotlin/com/ymougenel/myMetrics/controllers/impl/MetricController.kt
+++ src/main/kotlin/com/ymougenel/myMetrics/controllers/impl/MetricController.kt
@@ -6,11 +6,15 @@ import com.ymougenel.myMetrics.persistence.MetricsDAO
 import io.swagger.annotations.Api
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.rest.webmvc.ResourceNotFoundException
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
 
 @CrossOrigin
-@Controller
+@RestController
+@EnableWebMvc
 @RequestMapping(path = arrayOf("/metrics"))
 @Api(description = "CRUD Metric Controller")
 class MetricController : MetricControllerI {
@@ -23,23 +27,29 @@ class MetricController : MetricControllerI {
         this.metricsDao = metricsDAO
     }
 
-    @PostMapping("/")
-    @ResponseBody
+    @PostMapping(path = arrayOf("/"), consumes = [MediaType.APPLICATION_JSON_VALUE] )
+    @ResponseStatus(HttpStatus.CREATED)
     override fun create(@RequestBody metric: Metric) {
         metricsDao.save(metric)
     }
 
-    @GetMapping("/{id}")
-    override fun get(id: Long): Metric {
+    @GetMapping(path = arrayOf("/{id}"), produces = [MediaType.APPLICATION_JSON_UTF8_VALUE])
+    override fun get(@PathVariable id: Long): Metric {
         return metricsDao.findById(id)
                 .orElseThrow { ResourceNotFoundException("Metrics with id ${id} not found") }
     }
 
-    @PutMapping("/")
+    @PutMapping(consumes = [MediaType.APPLICATION_JSON_VALUE], path = arrayOf("/dfdsf"))
     @ResponseBody
     override fun update(@RequestBody metric: Metric) {
         // TODO handle error
         metricsDao.save(metric)
+    }
+
+    @PutMapping(consumes = [MediaType.APPLICATION_JSON_VALUE], path = arrayOf("/"))
+    fun update2(@RequestBody metric: Metric) {
+        // TODO handle error
+//        metricsDao.save(metric)
     }
 
     @DeleteMapping("/{id}")
@@ -48,4 +58,5 @@ class MetricController : MetricControllerI {
         metricsDao.deleteById(id)
     }
 
+    @GetMapping()
 }
--- src/main/kotlin/com/ymougenel/myMetrics/persistence/MetricsDAO.kt
+++ src/main/kotlin/com/ymougenel/myMetrics/persistence/MetricsDAO.kt
@@ -1,10 +1,11 @@
 package com.ymougenel.myMetrics.persistence
 
 import com.ymougenel.myMetrics.models.Metric
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.repository.CrudRepository
 import java.time.LocalDateTime
 
-interface MetricsDAO : CrudRepository<Metric, Long> {
+interface MetricsDAO : JpaRepository<Metric, Long> {
     fun findByDate(date: LocalDateTime): List<Metric>
     fun findByType(type: String): List<Metric>
 
